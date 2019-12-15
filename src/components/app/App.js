f9b3195a15ca0215b9d7148da2e68b7f961dee19
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2019-12-15 23:51:38 +0100

    styling contact cards

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import logo from './../../resources/logo.svg';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  const [posts, setPost] = useState(null);
+  const [posts, setPost] = useState(false);
 
   useEffect(() =>{
     axios.get('http://localhost:3000/posts')
@@ -15,12 +19,24 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="flex-container">
       { posts ?
         posts.map(post => (
-          <div key = {post.id}>
-            {post.title}
-          </div>)
+          <Card key = {post.id}>
+            <CardContent>
+              <Typography variant="h5">
+                {post.title}
+              </Typography>
+              <Typography variant="subtitle1">
+                {post.author}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">EDIT</Button>
+              <Button size="small">DELETE</Button>
+            </CardActions>
+          </Card>
+          )
         ) : (
           <div>yeet</div>
         )
