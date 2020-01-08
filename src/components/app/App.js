c24d73f49b43cd2aa37d927fc5441c2fd368f109
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-01-08 01:36:56 +0100

    added prettier, clear

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,53 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import "./App.css";
 
 const BASE_URL = "http://localhost:3000/posts";
 
 function App() {
-
   const [posts, setPost] = useState(false);
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
-  useEffect(() =>{
-    axios.get(BASE_URL)
-    .then(response => {
+  useEffect(() => {
+    axios.get(BASE_URL).then(response => {
       setPost(response.data);
-    })
+    });
   }, []);
 
   function handleSubmit() {
-    console.log("subitted");
-    axios.post(BASE_URL, {
-      id: Date.now(),
-      title: username,
-      author: email
-    })
-    .then(response => {
-    })
+    const userEmpty = username === "";
+    const emailEmpty = email === "";
+
+    if (!userEmpty && !emailEmpty) {
+      console.log("subitted");
+      axios
+        .post(BASE_URL, {
+          id: Date.now(),
+          title: username,
+          author: email
+        })
+        .then(response => {});
+    }
+  }
+
+  function handleDelete() {}
+
+  function handleChange(event) {
+    event.target.name === "username"
+      ? setUsername(event.target.value)
+      : setEmail(event.target.value);
+  }
+
+  function handleClear() {
+    setUsername("");
+    setEmail("");
   }
 
   return (
     <div className="flex-container">
-      <form className="form" onSubmit={handleSubmit} noValidate autoComplete="off">
-        <TextField 
-          name="username" 
-          fullWidth 
-          label="Name" 
-          onChange={(event) => setUsername(event.target.value)}
+      <form
+        className="form"
+        onSubmit={handleSubmit}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          name="username"
+          fullWidth
+          label="Name"
+          value={username}
+          onChange={handleChange}
         />
-        <TextField 
-          name="email" 
-          fullWidth 
-          label="Email" 
-          onChange={(event) => setEmail(event.target.value)}
+        <TextField
+          name="email"
+          fullWidth
+          label="Email"
+          value={email}
+          onChange={handleChange}
         />
         <div className="form-buttons">
           <div className="button-margin">
@@ -55,34 +78,29 @@ function App() {
               Submit
             </Button>
           </div>
-          <div className="button-margin">
+          <div onClick={handleClear} className="button-margin">
             <Button className="button-clear" variant="contained">
               Clear
             </Button>
           </div>
         </div>
       </form>
-      { posts ?
+      {posts ? (
         posts.map(post => (
-          <Card key = {post.id}>
+          <Card key={post.id}>
             <CardContent>
-              <Typography variant="h5">
-                {post.title}
-              </Typography>
-              <Typography variant="subtitle1">
-                {post.author}
-              </Typography>
+              <Typography variant="h5">{post.title}</Typography>
+              <Typography variant="subtitle1">{post.author}</Typography>
             </CardContent>
             <CardActions>
               <Button size="small">EDIT</Button>
               <Button size="small">DELETE</Button>
             </CardActions>
           </Card>
-          )
-        ) : (
-          <div>yeet</div>
-        )
-      }
+        ))
+      ) : (
+        <div>yeet</div>
+      )}
     </div>
   );
 }
