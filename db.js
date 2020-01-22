173257f1c76a675416ff44f971caa85f5bc522da
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-01-22 16:35:22 +0100

    apollo GraphQL client

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 module.exports = {
     posts: [
-        { id: 1, title: "Lorem Ipsum", views: 254, user_id: 123 },
-        { id: 2, title: "Sic Dolor amet", views: 65, user_id: 456 },
+        { id: 1, username: "dat-boi", email: "boi-author" },
+        { id: 1578494067262, username: "LMFAO wHO cAREAS HAHA Xd", email: "artour.babaevsky@elo.hell" },
     ],
     users: [
         { id: 123, name: "John Doe" },
         { id: 456, name: "Jane Doe" }
     ],
-    comments: [
+    comments: [ 
         { id: 987, post_id: 1, body: "Consectetur adipiscing elit", date: new Date('2017-07-03') },
         { id: 995, post_id: 1, body: "Nam molestie pellentesque dui", date: new Date('2017-08-17') }
     ]
---
+++ src/components/app/GraphQLApp.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import ApolloClient from "apollo-boost";
+import "./App.css";
+
+const GRAPHQL_URL = "http://localhost:4000";
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URL,
+});
+
+function GraphQLApp() {
+  const [contacts, setContact] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactId, setId] = useState("");
+
+  useEffect(() => {
+    axios.get(API_BASE_URL).then(response => {
+      setContact(response.data);
+    });
+  }, []);
+
+  function handleSubmit() {
+    const contactExists = contacts.some(u => u.id === contactId);
+
+    if (contactExists) {
+      console.log("update" + contactId);
+      const contact = {
+        id: contactId,
+        username: username,
+        email: email
+      };
+      axios
+        .put(API_BASE_URL + "/" + contactId.toString(), contact)
+        .then(response => {});
+      handleClear();
+    } else {
+      const userEmpty = username === "";
+      const emailEmpty = email === "";
+      if (!userEmpty && !emailEmpty) {
+        console.log("subitted");
+        const contact = {
+          id: Date.now(),
+          username: username,
+          email: email
+        };
+        axios.post(API_BASE_URL, contact).then(response => {});
+      }
+    }
+  }
+
+  function handleDelete(contact) {
+    console.log(contact.id);
+    axios.delete(API_BASE_URL + "/" + contact.id.toString()).then(res => {
+      console.log("USER DELETED", res);
+      setContact(contacts.filter(u => u.id !== contact.id));
+    });
+  }
+
+  function handleChange(event) {
+    event.target.name === "username"
+      ? setUsername(event.target.value)
+      : setEmail(event.target.value);
+  }
+
+  function handleClear() {
+    setUsername("");
+    setEmail("");
+    setId("");
+  }
+
+  return (
+    <div className="flex-container">
+      <form
+        className="form"
+        onSubmit={handleSubmit}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          name="username"
+          fullWidth
+          label="Name"
+          value={username}
+          onChange={handleChange}
+        />
+        <TextField
+          name="email"
+          fullWidth
+          label="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <div className="form-buttons">
+          <div className="button-margin">
+            <Button className="button-submit" variant="contained" type="submit">
+              Submit
+            </Button>
+          </div>
+          <div className="button-margin">
+            <Button
+              onClick={handleClear}
+              className="button-clear"
+              variant="contained"
+            >
+              Clear
+            </Button>
+          </div>
+        </div>
+      </form>
+      {contacts ? (
+        contacts.map(contact => (
+          <Card key={contact.id}>
+            <CardContent>
+              <Typography variant="h5">{contact.username}</Typography>
+              <Typography variant="subtitle1">{contact.email}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                onClick={() => {
+                  setUsername(contact.username);
+                  setEmail(contact.email);
+                  setId(contact.id);
+                }}
+                size="small"
+              >
+                EDIT
+              </Button>
+              <Button onClick={() => handleDelete(contact)} size="small">
+                DELETE
+              </Button>
+            </CardActions>
+          </Card>
+        ))
+      ) : (
+        <div>yeet</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
